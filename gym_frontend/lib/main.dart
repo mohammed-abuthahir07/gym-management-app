dbc7cffdfbc168862883c76daf66ced7dc46b2cc
Mohammed-Abuthahir <mohammedabuthahir316@gmail.com>
2026-09-09 14:57:03 +0530

    feat : flutter

--- gym_frontend/lib/main.dart
+++ gym_frontend/lib/main.dart
@@ -14,9 +14,31 @@ class GymApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Gym Management System'),
+          centerTitle: true,
         ),
-        body: const Center(
-          child: Text('Welcome to PeakForge Gym'),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // 1. Add your image here
+              Image.asset(
+                'assets/register.png', // Replace with your exact image filename
+                width: 250,
+                height: 250,
+                fit: BoxFit.cover,
+              ),
+              
+              const SizedBox(height: 20), // Adds space between image and text
+              
+              const Text(
+                'Welcome to PeakForge Gym',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
